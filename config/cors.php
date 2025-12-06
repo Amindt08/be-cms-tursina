628fc6c60d0b95b2c0f6fab2419849916ba58c17
AMINDTMSTR\User <amindtmstr>
2025-12-06 20:37:34 +0700

    no message

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,14 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:3001', 'http://127.0.0.1:3001'], // Ganti dengan domain frontend Anda
+    'allowed_origins' => [
+        'https://tursinakebab.id',
+        'https://www.tursinakebab.id',
+        'https://cms.tursinakebab.id',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, 
+    'supports_credentials' => true,
 ];
